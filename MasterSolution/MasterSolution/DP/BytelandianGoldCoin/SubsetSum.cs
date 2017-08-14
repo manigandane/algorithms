fae137c27c473aca0de8a1966bed057e4c4a7710
manigandan.elumalai <manigandan.elumalai@outlook.com>
2017-08-14 15:22:02 +0530

    solved subset sum problem

---
+++ MasterSolution/MasterSolution/DP/BytelandianGoldCoin/SubsetSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterSolution.DP.BytelandianGoldCoin
+{
+    public class SubsetSum
+    {
+        public static bool DoesSubsetExist(int[] setOfNos, int sum)
+        {
+            bool[,] tempResult = new bool[setOfNos.Length + 1, sum + 1]; // by default all will set to false;
+
+            for (int i = 0; i < setOfNos.Length; i++)
+            {
+                tempResult[i, 0] = true;
+            }
+
+            //for (int j = 0; j < sum; j++)
+            //{
+            //    tempResult[0, j] = false;
+            //}
+
+            for (int i = 1; i <= setOfNos.Length; i++)
+            {
+                for (int j = 1; j <= sum; j++)
+                {
+                    if (tempResult[i - 1, j])
+                    {
+                        tempResult[i, j] = tempResult[i - 1, j];
+                    }
+                    else if(setOfNos[i-1] <= j)
+                    {
+                        tempResult[i, j] = tempResult[i - 1, j - setOfNos[i-1]];
+                    }
+                }
+            }
+
+            return tempResult[setOfNos.Length, sum];
+        }
+    }
+}
--- MasterSolution/MasterSolution/Program.cs
+++ MasterSolution/MasterSolution/Program.cs
@@ -7,11 +7,13 @@ namespace MasterSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Result of 9 is {ByteLandian.GetDollars(9)}");
-            Console.WriteLine($"Result of 19 is {ByteLandian.GetDollars(19)}");
-            Console.WriteLine($"Result of 25 is {ByteLandian.GetDollars(25)}");
-            Console.WriteLine($"Result of 520 is {ByteLandian.GetDollars(520)}");
+            //Console.WriteLine($"Result of 9 is {ByteLandian.GetDollars(9)}");
+            //Console.WriteLine($"Result of 19 is {ByteLandian.GetDollars(19)}");
+            //Console.WriteLine($"Result of 25 is {ByteLandian.GetDollars(25)}");
+            //Console.WriteLine($"Result of 520 is {ByteLandian.GetDollars(520)}");
 
+            Console.WriteLine($"Subset of sum 17 in [2,4,6,9] exist?: {SubsetSum.DoesSubsetExist(new int[] { 2, 4, 6, 9 }, 17)}");
+            Console.WriteLine($"Subset of sum 17 in [2,4,6,8] exist?: {SubsetSum.DoesSubsetExist(new int[] { 2, 4, 6, 8 }, 17)}");
             Console.ReadLine();
         }
     }
